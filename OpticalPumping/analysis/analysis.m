899e22d052f71b0f74a8fc0dc31c0322f06cb677
Christopher Sarabalis <C.Sarabalis@gmail.com>
2013-02-28 22:31:25 -0500

    tableH stuff

--- OpticalPumping/analysis/analysis.m
+++ OpticalPumping/analysis/analysis.m
@@ -6,24 +6,20 @@
 %		contains sweep rate error analysis. data collected by varying
 %		the sweep rate and finding the absorption peak
 
-%tableB [ 1:Current in the X [mA], 2:Current in the Y [mA], 3:Current in the Z [mA],
-%         4:Magnetic Field as Measured by the Magnetometer [mG],
-%         5:Resonance Frequency of the First Peak [s], 6:Resonance
-%         Frequency of the Second%tableD [ RF Coil Frequency [kHz], First
-%         Peak Resonance Frequency [mV], Second Peak Frequency [mV],
-%         Instrument Precision for Peak 1 [mv], Instrument Precision for
-%         Second Peak [mV], Discrepancy Between Measurements by Chris and
-%         Tom for Peak 1 [mV], Discrepancy for Peak 2 [mV] ]
+%tableB   [ 1:Current in the X [mA], 
+%           2:Current in the Y [mA],
+%           3:Current in the Z [mA],
+%           4:Magnetic Field as Measured by the Magnetometer [mG],
+%           5:Resonance Frequency of the First Peak [s],
+%           6:Resonance Frequency of the Second Peak [s],
+%           7:Beginning of Sweep [kHz],
+%           8: End of Sweep [kHz],
+%           9: Duration of Sweep [s] ]
 %
 %       Description:
-%       fixed frequency, sweep B Peak [s],
-%         7:Beginning of Frequency Sweep [kHz],
-%         8:End of Frequency Sweep [kHz],
-%         9:Duration of Sweep [s]  ]
+%         fixed magnetic field, sweeping coil drive frequency
 %
-%       Description:
-%		contains resonance frequencies for varying magnetic field
-%		along the z axis
+%       Date: 2/15/13
 
 %tableC [ Average Voltage as Measured by the Oscilloscope [mV], 
 %         Current Measure by the Multimeter [mA] ]
@@ -41,15 +37,17 @@
 %       fixed frequency, sweep B
 %       driven by a ramp
 
-%tableE [ RF Coil Frequency [kHz], First Peak Resonance Frequency [mV],
-%         Second Peak Frequency [mV], Instrument Precision for Peak 1 [mv],
-%         Instrument Precision for Second Peak [mV], Discrepancy Between
-%         Measurements by Chris and Tom for Peak 1 [mV], Discrepancy for
-%         Peak 2 [mV] ]
+%tableE   [ RF Coil Frequency [kHz],
+%           First Peak Resonance Frequency [mV],
+%           Second Peak Frequency [mV],
+%           Instrument Precision for Peak 1 [mv],
+%           Instrument Precision for Second Peak [mV],
+%           Discrepancy Between Measurements by Chris and Tom for Peak 1 [mV],
+%           Discrepancy for Peak 2 [mV] ]
 %
 %       Description:
-%       fixed frequency, sweep B
-%       driven by a triangle wave
+%         fixed frequency, sweep B
+%         driven by a triangle wave
 
 %tableF [ 1:Current in the X [mA], 2:Current in the Y [mA], 3:Current in the Z [mA],
 %         4:Magnetic Field as Measured by the Magnetometer [mG],
@@ -67,13 +65,19 @@
 %       Temperature of vapour chamber: 42.4 deg celsius
 %       Date recorded: Mon 25 Feb
 
-%tableG [ 1:RF Coil Frequency [kHz], 2:Chris Peak 1 Resonance Frequency [mV],
-%         3:Chris Peak 2 Frequency [mV], 4:Tom Peak 1 [mV], 5:Tom Peak 2
-%         [mV], 6:Instrument Precision for First Peak Chris [mv], 7:Instrument
-%         Precision for Second Peak Chris [mV], 8:Sweep Range [Volts
-%         peak2peak], 9:Sweep time [secs], 10:Sweep Offset [Volts],
-%         11:[Up-ramp=1, down-ramp=0], 12:Instrument Precision for First Peak
-%         Tom [mV], 13:Instrument prec for second peak Tom [mV]
+%tableG   [ 1:RF Coil Frequency [kHz],
+%           2:Chris Peak 1 Resonance Frequency [mV],
+%           3:Chris Peak 2 Frequency [mV],
+%           4:Tom Peak 1 [mV],
+%           5:Tom Peak 2 [mV],
+%           6:Instrument Precision for First Peak Chris [mv],
+%           7:Instrument Precision for Second Peak Chris [mV],
+%           8:Sweep amplitude [Vpp],
+%           9:Sweep time [s],
+%           10:Sweep offset [V],
+%           11:[Up-ramp=1, down-ramp=0],
+%           12:Instrument Precision for First Peak Tom [mV],
+%           13:Instrument prec for second peak Tom [mV] ]
 %
 %       Description:
 %       fixed frequency, sweep B
@@ -88,6 +92,21 @@
 %
 %       Temperature of vapour chamber: 42.1 deg celsius
 %       Date recorded: Mon 25 Feb
+
+%tableB   [ 1:Current in the X [mA], 
+%           2:Current in the Y [mA],
+%           3:Current in the Z [mA],
+%           4:Magnetic Field as Measured by the Magnetometer [mG],
+%           5:Resonance Frequency of the First Peak [s],
+%           6:Resonance Frequency of the Second Peak [s],
+%           7:Beginning of Sweep [kHz],
+%           8: End of Sweep [kHz],
+%           9: Duration of Sweep [s] ]
+%
+%       Description:
+%         fixed magnetic field, sweeping coil drive frequency
+%
+%       Date: 2/27/13
 
 home = pwd;
 
@@ -136,7 +155,7 @@ indices = ~isnan(tableB(:,5));
 resFreqOne = tableB(indices,5)./tableB(indices,9); %convert into fraction of sweep
 resFreqOne = tableB(indices,7) + resFreqOne.*(tableB(indices,8)-tableB(indices,7)); %convert into kHz
 display('First Peak Frequency vs. B')
-fitB1 = linearFit(Btotal(indices),resFreqOne - 2*fitA.a*100,ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
+fitB1 = linearFit(Btotal(indices),resFreqOne - 2*fitA.a*(tableB(indices,8)-tableB(indices,7))./tableB(indices,9),ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
 %plot model
 hold on
 plot(Btotal(indices),466.54*Btotal(indices),'-r')
@@ -154,7 +173,7 @@ indices = ~isnan(tableB(:,6));
 resFreqTwo = tableB(indices,6)./tableB(indices,9); %convert into fraction of sweep
 resFreqTwo = tableB(indices,7) + resFreqTwo.*(tableB(indices,8)-tableB(indices,7)); %convert into kHz
 display('First Peak Frequency vs. B')
-fitB2 = linearFit(Btotal(indices),resFreqTwo - 2*fitA.a*100,ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
+fitB2 = linearFit(Btotal(indices),resFreqTwo - 2*fitA.a*(tableB(indices,8)-tableB(indices,7))./tableB(indices,9),ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
 %plot model
 hold on
 plot(Btotal(indices),699.81*Btotal(indices),'-r')
@@ -166,6 +185,62 @@ fontSize(16)
 %print 
 display('Expected g*mu_B/h = 6.998123e9')
 display(['Measured: ',num2str(fitB2.a*1e7,'%10.5e'),' \pm ', num2str(fitB2.aerr*1e7,'%10.5e')])
+
+%cleanup
+clear indices resFreqOne resFreqTwo Btotal
+
+%% Repeated Analysis Cell for Table H: Fixed B, Sweep Frequency
+
+Btotal = sqrt(i2b(tableH(:,1) - IEarth(1),'x').^2+i2b(tableH(:,2)-IEarth(2),'y').^2+i2b(tableH(:,3)-IEarth(3),'z').^2);
+%plot the total B field calculated from Helmholtz currents and measured by
+%   the magnetometer
+figure
+plot(Btotal,tableH(:,4)*0.001,'.')
+title('Magnetic Field Consistency Check')
+xlabel('Magnitude of B Calculated from Currents [G]')
+ylabel('B Measured by Magnetometer [G]')
+%seems to be a mismatch between positive and negative readings
+figure
+plot(Btotal,abs(tableH(:,4)*0.001),'.')
+title('Magnetic Field Consistency Check')
+xlabel('Magnitude of B Calculated from Currents [G]')
+ylabel('Magnitude of B Measured by Magnetometer [G]')
+
+% First Peak
+indices = ~isnan(tableH(:,5));
+resFreqOne = tableH(indices,5)./tableH(indices,9); %convert into fraction of sweep
+resFreqOne = tableH(indices,7) + resFreqOne.*(tableH(indices,8)-tableH(indices,7)); %convert into kHz
+display('First Peak Frequency vs. B')
+fitH1 = linearFit(Btotal(indices),resFreqOne - 2*fitA.a*(tableH(indices,8)-tableH(indices,7))./tableH(indices,9),ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
+%plot model
+hold on
+plot(Btotal(indices),466.54*Btotal(indices),'-r')
+%annotate
+title('Resonsnace Frequency of Peak 1 vs. Magnetic Field Strength')
+xlabel('Total Magnetic Field [G]')
+ylabel('Resonance Frequency [kHz]')
+fontSize(16)
+%print results
+display('Expected g*mu_B/h = 4.665415e9')
+display(['Measured: ',num2str(fitH1.a*1e7,'%10.5e'),' \pm ', num2str(fitH1.aerr*1e7,'%10.5e')])
+
+% Second Peak
+indices = ~isnan(tableH(:,6));
+resFreqTwo = tableH(indices,6)./tableH(indices,9); %convert into fraction of sweep
+resFreqTwo = tableH(indices,7) + resFreqTwo.*(tableH(indices,8)-tableH(indices,7)); %convert into kHz
+display('First Peak Frequency vs. B')
+fitH2 = linearFit(Btotal(indices),resFreqTwo - 2*fitA.a*(tableH(indices,8)-tableH(indices,7))./tableH(indices,9),ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
+%plot model
+hold on
+plot(Btotal(indices),699.81*Btotal(indices),'-r')
+%annotate
+title('Resonsnace Frequency of Peak 2 vs. Magnetic Field Strength')
+xlabel('Total Magnetic Field [G]')
+ylabel('Resonance Frequency [kHz]')
+fontSize(16)
+%print 
+display('Expected g*mu_B/h = 6.998123e9')
+display(['Measured: ',num2str(fitH2.a*1e7,'%10.5e'),' \pm ', num2str(fitH2.aerr*1e7,'%10.5e')])
 
 %cleanup
 clear indices resFreqOne resFreqTwo Btotal

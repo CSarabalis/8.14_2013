a60f1db5e1b9ac425516aa0a4267ece80f6c6891
Christopher Sarabalis <C.Sarabalis@gmail.com>
2013-03-15 15:44:18 -0400

    who knows at this point

---
+++ DopplerFree/analysis/tools/cjsImport.m
@@ -0,0 +1,39 @@
+function [cellArrayofStruc] = cjsImport(fileType,delimiter,headerLines)
+%cjsImport imports all .fileType in pwd and puts them in a cell array containing
+%the fileName
+%
+%   Assumptions:
+%       the file starts with an index
+%       
+
+%scrub inputs
+if not(ischar(fileType))
+    error('Invalid Argument: Input is not a string.');
+end    
+
+%grab the file names
+files = regexp(ls,['[-\.\w]*\.',fileType],'match');
+%grab the file indices
+index = regexp(files, '^([0-9]*)','tokens');
+display(index)
+%convert files indices into integers
+for i = 1:length(index)
+    indices(i) = str2num(index{i}{1}{1});
+end
+%sort files by index
+[~,IX] = sort(indices);
+files = files(IX);
+
+if (size(files)==[0,0])   %checks for matches
+    cellArrayofStruc = 0;
+    disp(['No ',fileType,' in pwd']);
+else                      %import the data into a cell array
+    for i = 1:length(files)
+        x.data = importdata(files{i},delimiter,headerLines);
+        x.data = x.data.data;
+        x.fileName = files{i};
+        cellArrayofStruc{i} = x;
+    end
+end
+
+end
--- OpticalPumping/analysis/subscripts/fixBvaryF.m
+++ OpticalPumping/analysis/subscripts/fixBvaryF.m
@@ -1,4 +1,39 @@
+%% Table B analysis
+
+%compute the total magnetic field
 Btotal = sqrt(i2b(tableB(:,1) - IEarth(1),'x').^2+i2b(tableB(:,2)-IEarth(2),'y').^2+i2b(tableB(:,3)-IEarth(3),'z').^2);
+
+%compute the uncertainty in the frequency_______________________
+
+%time      (y-axis)
+sigInstrumentPrecision = 1; %ms
+sigStatPeak1 = 6.19; %ms
+sigStatPeak2 = 3.54; %ms
+numberOfSweeps = 8;
+sigXatMax = 10; %ms
+sigSweepRateCorrectionError = fitA.aerr; %s
+%mag field (x-axis)
+sigi = sqrt(0.1^2+0.1^2); %0.1 mA from precision, 0.1 mA from meter discrepancy
+sigRelCoilRadius = 0.25./[7.3 8.9 11.0]; %measurements in inches
+sigIEarth = [1.8 0.29 0.40]; %mA
+
+tableB(:,10) =sqrt(sigInstrumentPrecision^2 + sigStatPeak1^2/numberOfSweeps +...
+    sigXatMax^2 + (sigSweepRateCorrectionError.*(tableB(:,8)-tableB(:,7))./tableB(:,9)).^2)...
+    *0.001.*(tableB(:,8)-tableB(:,7))./tableB(:,9)+...
+    469.6848*sqrt(i2b(tableB(:,1) - IEarth(1),'x').^4.*((sigi^2+sigIEarth(1)^2)./(tableB(:,1) - IEarth(1)).^2+sigRelCoilRadius(1)^2)+...
+    i2b(tableB(:,2) - IEarth(2),'y').^4.*((sigi^2+sigIEarth(2)^2)./(tableB(:,2) - IEarth(2)).^2+sigRelCoilRadius(2)^2)+...
+    i2b(tableB(:,3) - IEarth(3),'z').^4.*((sigi^2+sigIEarth(3)^2)./(tableB(:,3) - IEarth(3)).^2+sigRelCoilRadius(3)^2))./Btotal;
+
+tableB(:,11) =sqrt(sigInstrumentPrecision^2 + sigStatPeak1^2/numberOfSweeps +...
+    sigXatMax^2 + (sigSweepRateCorrectionError.*(tableB(:,8)-tableB(:,7))./tableB(:,9)).^2)...
+    *0.001.*(tableB(:,8)-tableB(:,7))./tableB(:,9)+...
+    705.2103*sqrt(i2b(tableB(:,1) - IEarth(1),'x').^4.*((sigi^2+sigIEarth(1)^2)./(tableB(:,1) - IEarth(1)).^2+sigRelCoilRadius(1)^2)+...
+    i2b(tableB(:,2) - IEarth(2),'y').^4.*((sigi^2+sigIEarth(2)^2)./(tableB(:,2) - IEarth(2)).^2+sigRelCoilRadius(2)^2)+...
+    i2b(tableB(:,3) - IEarth(3),'z').^4.*((sigi^2+sigIEarth(3)^2)./(tableB(:,3) - IEarth(3)).^2+sigRelCoilRadius(3)^2))./Btotal;
+
+
+
+%%
 % %plot the total B field calculated from Helmholtz currents and measured by
 % %   the magnetometer
 % figure
@@ -21,7 +56,7 @@ subplot(2,2,1)
 	resFreqOne = tableB(indices,5)./tableB(indices,9); %convert into fraction of sweep
 	resFreqOne = tableB(indices,7) + resFreqOne.*(tableB(indices,8)-tableB(indices,7)); %convert into kHz
     display('First Peak Frequency vs. B')
-	fitB1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(tableB(indices,8)-tableB(indices,7))./tableB(indices,9),ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate
+	fitB1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(tableB(indices,8)-tableB(indices,7))./tableB(indices,9),tableB(indices,10)) %correction in arg2: 200 kHz is the sweep rate
 	%plot model
     hold on
 	plot(Btotal(indices),466.54*Btotal(indices),'-r')
@@ -40,7 +75,7 @@ subplot(2,2,3)
 	resFreqTwo = tableB(indices,6)./tableB(indices,9); %convert into fraction of sweep
 	resFreqTwo = tableB(indices,7) + resFreqTwo.*(tableB(indices,8)-tableB(indices,7)); %convert into kHz
 	display('First Peak Frequency vs. B')
-	fitB2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(tableB(indices,8)-tableB(indices,7))./tableB(indices,9),ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate
+	fitB2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(tableB(indices,8)-tableB(indices,7))./tableB(indices,9),tableB(indices,11)) %correction in arg2: 200 kHz is the sweep rate
 	%plot model
 	hold on
 	plot(Btotal(indices),699.81*Btotal(indices),'-r')
@@ -54,7 +89,23 @@ subplot(2,2,3)
     display(['Measured: ',num2str(fitB2.a*1e7,'%10.5e'),' \pm ', num2str(fitB2.aerr*1e7,'%10.5e')])
 
 Btotal = sqrt(i2b(tableH(:,1) - IEarth(1),'x').^2+i2b(tableH(:,2)-IEarth(2),'y').^2+i2b(tableH(:,3)-IEarth(3),'z').^2);
-    
+
+numberOfSweeps = 32;
+
+tableH(:,10) =sqrt(sigInstrumentPrecision^2 + sigStatPeak1^2/numberOfSweeps +...
+    sigXatMax^2 + (sigSweepRateCorrectionError.*(tableH(:,8)-tableH(:,7))./tableH(:,9)).^2)...
+    *0.001.*(tableH(:,8)-tableH(:,7))./tableH(:,9)+...
+    469.6848*sqrt(i2b(tableH(:,1) - IEarth(1),'x').^4.*((sigi^2+sigIEarth(1)^2)./(tableH(:,1) - IEarth(1)).^2+sigRelCoilRadius(1)^2)+...
+    i2b(tableH(:,2) - IEarth(2),'y').^4.*((sigi^2+sigIEarth(2)^2)./(tableH(:,2) - IEarth(2)).^2+sigRelCoilRadius(2)^2)+...
+    i2b(tableH(:,3) - IEarth(3),'z').^4.*((sigi^2+sigIEarth(3)^2)./(tableH(:,3) - IEarth(3)).^2+sigRelCoilRadius(3)^2))./Btotal;
+
+tableH(:,11) =sqrt(sigInstrumentPrecision^2 + sigStatPeak1^2/numberOfSweeps +...
+    sigXatMax^2 + (sigSweepRateCorrectionError.*(tableH(:,8)-tableH(:,7))./tableH(:,9)).^2)...
+    *0.001.*(tableH(:,8)-tableH(:,7))./tableH(:,9)+...
+    705.2103*sqrt(i2b(tableH(:,1) - IEarth(1),'x').^4.*((sigi^2+sigIEarth(1)^2)./(tableH(:,1) - IEarth(1)).^2+sigRelCoilRadius(1)^2)+...
+    i2b(tableH(:,2) - IEarth(2),'y').^4.*((sigi^2+sigIEarth(2)^2)./(tableH(:,2) - IEarth(2)).^2+sigRelCoilRadius(2)^2)+...
+    i2b(tableH(:,3) - IEarth(3),'z').^4.*((sigi^2+sigIEarth(3)^2)./(tableH(:,3) - IEarth(3)).^2+sigRelCoilRadius(3)^2))./Btotal;
+
 % %plot the total B field calculated from Helmholtz currents and measured by
 % %   the magnetometer
 % figure
@@ -75,7 +126,7 @@ subplot(2,2,2)
 	resFreqOne = tableH(indices,5)./tableH(indices,9); %convert into fraction of sweep
 	resFreqOne = tableH(indices,7) + resFreqOne.*(tableH(indices,8)-tableH(indices,7)); %convert into kHz
 	display('First Peak Frequency vs. B')
-	fitH1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(tableH(indices,8)-tableH(indices,7))./tableH(indices,9),ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate
+	fitH1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(tableH(indices,8)-tableH(indices,7))./tableH(indices,9),tableH(indices,10)) %correction in arg2: 200 kHz is the sweep rate
 	%plot model
 	hold on
 	plot(Btotal(indices),466.54*Btotal(indices),'-r')
@@ -94,7 +145,7 @@ subplot(2,2,4)
 	resFreqTwo = tableH(indices,6)./tableH(indices,9); %convert into fraction of sweep
 	resFreqTwo = tableH(indices,7) + resFreqTwo.*(tableH(indices,8)-tableH(indices,7)); %convert into kHz
 	display('First Peak Frequency vs. B')
-	fitH2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(tableH(indices,8)-tableH(indices,7))./tableH(indices,9),ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate
+	fitH2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(tableH(indices,8)-tableH(indices,7))./tableH(indices,9),tableH(indices,11)) %correction in arg2: 200 kHz is the sweep rate
 	%plot model
 	hold on
 	plot(Btotal(indices),699.81*Btotal(indices),'-r')
@@ -117,7 +168,7 @@ Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(
 	display('First Peak Frequency vs. B')
     figure
     subplot(2,1,1)
-	fitResFixB1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
+	fitResFixB1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),x(indices,10)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
 	%plot model
 	hold on
 	plot(Btotal(indices),466.54*Btotal(indices),'-r')
@@ -141,7 +192,7 @@ Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(
 	display('Second Peak Frequency vs. B')
     figure
     subplot(2,1,1)
-	fitResFixB2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate
+	fitResFixB2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),x(indices,11)) %correction in arg2: 200 kHz is the sweep rate
 	%plot model
 	hold on
 	plot(Btotal(indices),699.81*Btotal(indices),'-r')
@@ -162,6 +213,21 @@ Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(
     
 x = [tableI];
 Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(:,3)-IEarth(3),'z').^2);
+
+tableI(:,10) =sqrt(sigInstrumentPrecision^2 + sigStatPeak1^2/numberOfSweeps +...
+    sigXatMax^2 + (sigSweepRateCorrectionError.*(tableI(:,8)-tableI(:,7))./tableI(:,9)).^2)...
+    *0.001.*(tableI(:,8)-tableI(:,7))./tableI(:,9)+...
+    469.6848*sqrt(i2b(tableI(:,1) - IEarth(1),'x').^4.*((sigi^2+sigIEarth(1)^2)./(tableI(:,1) - IEarth(1)).^2+sigRelCoilRadius(1)^2)+...
+    i2b(tableI(:,2) - IEarth(2),'y').^4.*((sigi^2+sigIEarth(2)^2)./(tableI(:,2) - IEarth(2)).^2+sigRelCoilRadius(2)^2)+...
+    i2b(tableI(:,3) - IEarth(3),'z').^4.*((sigi^2+sigIEarth(3)^2)./(tableI(:,3) - IEarth(3)).^2+sigRelCoilRadius(3)^2))./Btotal;
+
+tableI(:,11) =sqrt(sigInstrumentPrecision^2 + sigStatPeak1^2/numberOfSweeps +...
+    sigXatMax^2 + (sigSweepRateCorrectionError.*(tableI(:,8)-tableI(:,7))./tableI(:,9)).^2)...
+    *0.001.*(tableI(:,8)-tableI(:,7))./tableI(:,9)+...
+    705.2103*sqrt(i2b(tableI(:,1) - IEarth(1),'x').^4.*((sigi^2+sigIEarth(1)^2)./(tableI(:,1) - IEarth(1)).^2+sigRelCoilRadius(1)^2)+...
+    i2b(tableI(:,2) - IEarth(2),'y').^4.*((sigi^2+sigIEarth(2)^2)./(tableI(:,2) - IEarth(2)).^2+sigRelCoilRadius(2)^2)+...
+    i2b(tableI(:,3) - IEarth(3),'z').^4.*((sigi^2+sigIEarth(3)^2)./(tableI(:,3) - IEarth(3)).^2+sigRelCoilRadius(3)^2))./Btotal;
+
     %First Peak_______________
 	indices = ~isnan(x(:,5));
 	resFreqOne = x(indices,5)./x(indices,9); %convert into fraction of sweep
@@ -169,7 +235,7 @@ Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(
 	display('DC Coupled: First Peak Frequency vs. B')
     figure
     subplot(2,1,1)
-	fitI1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),ones(length(resFreqOne),1)) %correction in arg2: 200 kHz is the sweep rate; mysterious factor of 2
+	fitI1 = linearFit(Btotal(indices),resFreqOne - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),x(indices,10)) 
 	%plot model
 	hold on
 	plot(Btotal(indices),466.54*Btotal(indices),'-r')
@@ -193,7 +259,7 @@ Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(
 	display('DC Coupled: Second Peak Frequency vs. B')
     figure
     subplot(2,1,1)
-	fitI2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),ones(length(resFreqTwo),1)) %correction in arg2: 200 kHz is the sweep rate
+	fitI2 = linearFit(Btotal(indices),resFreqTwo - fitA.a*(x(indices,8)-x(indices,7))./x(indices,9),x(indices,11)) %correction in arg2: 200 kHz is the sweep rate
 	%plot model
 	hold on
 	plot(Btotal(indices),699.81*Btotal(indices),'-r')
@@ -209,7 +275,22 @@ Btotal = sqrt(i2b(x(:,1) - IEarth(1),'x').^2+i2b(x(:,2)-IEarth(2),'y').^2+i2b(x(
 	%print 
 	display('Expected g*mu_B/h = 6.998123e9')
 	display(['Measured: ',num2str(fitI2.a*1e7,'%10.5e'),' \pm ', num2str(fitI2.aerr*1e7,'%10.5e')])
+    
+%% Statistical Variation in the Determination of the Peaks
+peaks = zeros(8,2);
+j = 0;
+for i = [13:19,21]
+    j = j+1;
+    [~,indices] = findpeaks(data{i}.data(:,2),'MINPEAKHEIGHT',.0005,'MINPEAKDISTANCE',100);
+    peaks(j,1:2) = indices';
+end
+[~,averagedPeaks] = findpeaks(data{i}.data(:,2),'MINPEAKHEIGHT',.002,'MINPEAKDISTANCE',100);
+averagedPeaks = averagedPeaks';
+display(['Sample Standard Deviation of Peak 1 from 8 runs: ',num2str(std(peaks(:,1)))]);
+display(['Sample Standard Deviation of Peak 2 from 8 runs: ',num2str(std(peaks(:,2)))]);
 %% Final Steps
     
 %cleanup
-	clear indices resFreqOne resFreqTwo Btotal x
+	clear indices resFreqOne resFreqTwo Btotal x i j indices peaks averagedPeaks
+    clear sigIEarth sigRelCoilRadius sigi sigSweepRateCorrectionError sigXatMax numberOfSweeps
+    clear sigStatPeak1 sigStatPeak2 sigInstrumentPrecision

626b18fb42c7118889e519d428113b100678793a
Thomas Alcorn <talcorn.92@gmail.com>
2013-04-12 07:02:08 -0400

    omg so tired

--- DopplerFree/analysis/alcorn.m
+++ DopplerFree/analysis/alcorn.m
@@ -22,7 +22,7 @@ clear i new_range
 
 %% input peak and FP data from raw
 
-i=90;
+i=91;
 
 % first plot up some data
 figure()
@@ -51,6 +51,134 @@ end
 
 clear i saveQ
 
+%% decide which peaks are which
+
+peak1=struct;
+peak1.shifts = da{79}.shifts;
+peak1.shiftsDiff = [0;diff(peak1.shifts)];
+peak1.uncerts = (1/(2*pi))*[0 0.086 0.0015; %uncert in peak, uncert in sin params (fp res), uncert in fp res
+    0 0.1314 0.0015;
+    0.0641 0.0081 0.0005;
+    0 0.0571 0.0010;
+    0.2773 0.0332 0.0005;
+    0.3612 0.0888 0.0014];
+peak1.uncerts = (peak1.uncerts(:,1).^2 + peak1.uncerts(:,2).^2 + peak1.uncerts(:,3).^2).^(0.5);
+peak1.uncerts = 0.321*peak1.uncerts;
+
+peak4=struct;
+peak4.shifts = da{86}.shifts;
+peak4.shiftsDiff = [0;diff(peak4.shifts)];
+peak4.uncerts = (1/(2*pi))*[0.113 0.0059 0.0006;
+    0 0.0002 0.0005;
+    0 0.0012 0.0005;
+    0 0 0;
+    0.092 0.0068 0.0006;
+    0 0.0092 0.0007;
+    0 0 0];
+peak4.uncerts = (peak4.uncerts(:,1).^2 + peak4.uncerts(:,2).^2 + peak4.uncerts(:,3).^2).^(0.5);
+peak4.uncerts = 0.321*peak4.uncerts;
+
+peak3=struct;
+peak3.shifts = da{84}.shifts;
+peak3.shiftsDiff = [0;diff(peak3.shifts)];
+peak3.uncerts = (1/(2*pi))*[0.047 0.0124 0.0005;
+    0.0397 0.005 0.0005;
+    0.0479 0.0143 0.0006];
+peak3.uncerts = (peak3.uncerts(:,1).^2 + peak3.uncerts(:,2).^2 + peak3.uncerts(:,3).^2).^(0.5);
+peak3.uncerts = 0.321*peak3.uncerts;
+
+peak2=struct;
+peak2.shifts = da{82}.shifts;
+peak2.shiftsDiff = [0;diff(peak2.shifts)];
+peak2.uncerts = (1/(2*pi))*[0.1387 0.0277 0.0009;
+    0 0.0023 0.0005;
+    0.047 0.01 0.0005];
+peak2.uncerts = (peak2.uncerts(:,1).^2 + peak2.uncerts(:,2).^2 + peak2.uncerts(:,3).^2).^(0.5);
+peak2.uncerts = 0.3216*peak2.uncerts;
+
+
+%% model testing
+
+% A is the acef model
+% B is the abc model
+
+
+peak1.A = [peak1.shiftsDiff(2)+peak1.shiftsDiff(3);
+    peak1.shiftsDiff(4)+peak1.shiftsDiff(5);
+    peak1.shiftsDiff(6)];
+
+peak1.Aexp = [0.2667; 0.1569; 0.0722];
+
+peak1.Au = [peak1.uncerts(1)^2+peak1.uncerts(2)^2+peak1.uncerts(3)^2;
+    peak1.uncerts(4)^2+peak1.uncerts(5)^2+peak1.uncerts(3)^2;
+    peak1.uncerts(6)^2+peak1.uncerts(5)^2].^(0.5);
+
+peak1.B = [peak1.shiftsDiff(2);
+    peak1.shiftsDiff(3)];
+
+peak1.Bexp = [0.1569; 0.0722];
+
+peak1.Bu = [peak1.uncerts(1)^2+peak1.uncerts(2)^2;
+    peak1.uncerts(1)^2+peak1.uncerts(2)^2].^0.5;
+
+
+
+peak4.A = [peak4.shiftsDiff(2)+peak4.shiftsDiff(3);
+    peak4.shiftsDiff(4)+peak4.shiftsDiff(5);
+    peak4.shiftsDiff(6)];
+
+peak4.Aexp = peak1.Aexp;
+
+peak4.Au = [peak4.uncerts(1)^2+peak4.uncerts(2)^2+peak4.uncerts(3)^2;
+    peak4.uncerts(4)^2+peak4.uncerts(5)^2+peak4.uncerts(3)^2;
+    peak4.uncerts(6)^2+peak4.uncerts(5)^2].^(0.5);
+
+peak4.B = [peak4.shiftsDiff(2);
+    peak4.shiftsDiff(3)];
+
+peak4.Bexp = peak1.Bexp;
+
+peak4.Bu = [peak4.uncerts(1)^2+peak4.uncerts(2)^2;
+    peak4.uncerts(2)^2+peak4.uncerts(3)^2].^0.5;
+
+
+
+peak2.C = peak2.shiftsDiff(4);
+
+peak2.exp = [29.3]*10^-3;
+
+peak2.Cu = [peak2.uncerts(3)^2+peak2.uncerts(2)^2].^0.5;
+
+
+peak3.C = peak3.shiftsDiff([2 3]);
+
+peak3.exp = [63.4; 29.3]*10^-3;
+
+peak3.Cu = [peak3.uncerts(2)^2+peak3.uncerts(1)^2;
+    peak3.uncerts(2)^2+peak3.uncerts(3)^2].^0.5;
+    
+
+
+% evaluate p-values
+
+overallP = cdf(gmdistribution([peak1.Aexp' peak4.Aexp'],[peak1.Au' peak4.Au']),[peak1.A' peak4.A'])
+
+means = [peak1.Aexp; peak1.Bexp; peak2.exp; peak3.exp; peak4.Aexp; peak4.Bexp];
+stds = [peak1.Au; peak1.Bu; peak2.Cu; peak3.Cu; peak4.Au; peak4.Bu];
+datas = [peak1.A; peak1.B; peak2.C; peak3.C; peak4.A; peak4.B];
+
+pvals = [];
+for i=1:max(size(means))
+    pvals = [pvals; 2*cdf(gmdistribution(0,stds(i)^2),-abs(means(i)-datas(i)))];
+end
+
+peak1.Ap = pvals(1:3);
+peak1.Bp = pvals(4:5);
+peak2.Cp = pvals(6);
+peak3.Cp = pvals(7:8);
+peak4.Ap = pvals(9:11);
+peak4.Bp = pvals(12:13);
+
 
 %% look at raw data
 
@@ -72,26 +200,21 @@ clear i indices
 
 dummy = [];
 
-for i=79:84
+for i=90
     figure()
     dummy = [dummy fftPlot(da,i)];
 end
-
-
-
 
 clear i
 
 %% find peaks
 
-i=43;
+i=80;
 %plots the peaks overlaid with the calculated shifts. for reference
 figure()
 findPeaks(da,i)
 
 clear i
-
-%% analysis of peaks and comparison with expected values
 
 %% Rb87 analysis
 
--- DopplerFree/analysis/tools/findPeaks.m
+++ DopplerFree/analysis/tools/findPeaks.m
@@ -1,23 +1,25 @@
 function bananas = findPeaks(data,index)
-
+% fuck yourself cunt
 plot(data{index}.bal,'.')
-[x y] = ginput;
+title(data{index}.name)
+%[x y] = ginput;
 
 maxY = max(data{index}.bal);
 minY = min(data{index}.bal);
 
-shifts = data{index}.shifts;
-shifts = sort(shifts,1,'descend');
-
-shifts_length = abs(shifts(end)-shifts(1));
-x_length = abs(x(end)-x(1));
-grad = x_length/shifts_length;
-
-shifts_x = grad*shifts + x(1);
+shifts = sort(data{index}.peakIndices);
+shifts_x = shifts;
+% shifts = sort(shifts,1,'descend');
+% 
+% shifts_length = abs(shifts(end)-shifts(1));
+% x_length = abs(x(end)-x(1));
+% grad = x_length/shifts_length;
+% 
+% shifts_x = grad*shifts + x(1);
 
 for i=1:max(size(shifts_x))
     line([shifts_x(i) shifts_x(i)],[minY maxY])
-    text(shifts_x(i), maxY, num2str(shifts(i)))
+    text(shifts_x(i), maxY, num2str(data{index}.shiftsDiff(i)))
 end
 
 
